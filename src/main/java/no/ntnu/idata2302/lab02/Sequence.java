c21ee437dfd7885a60aff26a62728d670f7e742e
Franck Chauvel <fchauvel@Francks-MacBook-Air.local>
2023-09-05 06:06:03 +0200

    test: Suite for insert, remove, search, extrema, and hasDuplicate

--- src/main/java/no/ntnu/idata2302/lab02/Sequence.java
+++ src/main/java/no/ntnu/idata2302/lab02/Sequence.java
@@ -7,33 +7,122 @@
  */
 package no.ntnu.idata2302.lab02;
 
-
-
+/**
+ * Implement the Sequence ADT from Lecture 2.2
+ *
+ * The items in the sequence are indexed from 1 (as opposed to Java arrays that
+ * are indexed from 0)
+ */
 public class Sequence {
+
+    private static final int INITIAL_CAPACITY = 100;
 
     private int capacity;
     private int length;
+    private int[] items;
 
     public Sequence() {
-        this.capacity = 100;
-        this.length = length;
+        this(INITIAL_CAPACITY, new int[]{});
     }
 
+    public Sequence(int capacity, int[] items) {
+        if (capacity < items.length) {
+            throw new IllegalArgumentException("Capacity must be greater than item count");
+        }
+        this.capacity = capacity;
+        this.length = items.length;
+        this.items = new int[capacity];
+        for (int i=0 ; i<items.length ; i++) {
+            this.items[i] = items[i];
+        }
+    }
 
+    /**
+     * @return The number of items in the sequence
+     */
     public int getLength() {
         return this.length;
     }
 
-    public void insert(int index, int item) {
-        // TODO
+    /**
+     * @return the number of "buckets" currently allocated
+     */
+    int getCapacity() {
+        return this.capacity;
     }
 
-    public void remove(int index, int item) {
-        // TODO
+    /**
+     * Return the item stored at the given index
+     *
+     * @param index the index of the desired item, starting at 1
+     * @return the item at the given index.
+     */
+    public int get(int index) {
+        if (index < 1 || index > length) {
+            throw new IllegalArgumentException("Invalid index!");
+        }
+        return this.items[index - 1];
     }
 
-    public void search(int item) {
-        // TODO
+    /**
+     * Append the given item at the end of the sequence
+     *
+     * @param item the item that must be inserted
+     */
+    public void insert(int item, int index) {
+        // TODO: Implement
+        throw new RuntimeException("Not yet implemented.");
+    }
+
+    /**
+     * Remove the index at the given index
+     *
+     * @param index the index that must be removed.
+     */
+    public void remove(int index) {
+        // TODO: Implement
+        throw new RuntimeException("Not yet implemented.");
+    }
+
+    /**
+     * Find a index where the given item can be found. Returns 0 if that item cannot
+     * be found.
+     *
+     * @param item the item whose index must be found
+     * @return an
+     */
+    public int search(int item) {
+        // TODO: Implement
+        throw new RuntimeException("Not yet implemented.");
+    }
+
+    /**
+     * Find both the smallest and the largest items in the sequence.
+     *
+     * @return an array of length two where the first entry is the minimum and the
+     *         second the maximum
+     */
+    public int[] extrema() {
+        // TODO: Implement
+        throw new RuntimeException("Not yet implemented.");
+    }
+
+    /**
+     * Check whether the given sequence contains any duplicate item
+     *
+     * @return true if the sequence has the the same items at multiple indices
+     */
+    public boolean hasDuplicate() {
+        // TODO: Implement
+        throw new RuntimeException("Not yet implemented.");
+    }
+
+
+    /**
+     * Convert the sequence into an Java array
+     */
+    public int[] toArray() {
+        return items;
     }
 
 }
--- src/test/java/no/ntnu/idata2302/lab02/SequenceTest.java
+++ src/test/java/no/ntnu/idata2302/lab02/SequenceTest.java
@@ -21,4 +21,126 @@ public class SequenceTest
         assertEquals(0, sequence.getLength());
     }
 
+    @Test
+    public void insertionShouldIncreaseLength() {
+        var sequence = new Sequence();
+        sequence.insert(25, 1);
+        assertEquals(1, sequence.getLength());
+    }
+
+    @Test
+    public void insertShouldShiftItemsTowardsTheEnd () {
+        var sequence = new Sequence(10, new int[]{1,2,3});
+        sequence.insert(100, 2);
+        assertEquals(4, sequence.getLength());
+        assertEquals(1, sequence.get(1));
+        assertEquals(100, sequence.get(2));
+        assertEquals(2, sequence.get(3));
+        assertEquals(3, sequence.get(4));
+    }
+
+    @Test
+    public void insertShouldIncreaseCapacityWhenFull () {
+        var sequence = new Sequence(2, new int[]{10,20});
+        sequence.insert(30, 3);
+        assertEquals(3, sequence.getLength());
+        assertEquals(4, sequence.getCapacity());
+    }
+
+    @Test
+    public void searchShouldReturnZeroWhenTheItemDoesNotOccur () {
+        var sequence = new Sequence(10, new int[]{1,2,3});
+        var index = sequence.search(4);
+        assertEquals(0, index);
+    }
+
+    @Test
+    public void searchShouldReturnAIndexWhereItemIs () {
+        var sequence = new Sequence(10, new int[]{1,2,3,4,5});
+        assertEquals(3, sequence.search(3));
+    }
+
+    @Test
+    public void searchShouldReturnZeroWhenTheSequenceIsEmpty() {
+        var sequence = new Sequence(10, new int[]{});
+        assertEquals(0, sequence.search(23));
+    }
+
+    @Test(expected=IllegalArgumentException.class)
+    public void removeShouldRejectEmptySequences() {
+        var sequence = new Sequence(2, new int[]{});
+        sequence.remove(2);
+    }
+
+    @Test
+    public void removeShouldReduceTheLength() {
+        var sequence = new Sequence(4, new int[]{1,2,3});
+        sequence.remove(2);
+        assertEquals(2, sequence.getLength());
+    }
+
+    @Test
+    public void removeShouldShiftElementsTowardsTheBeginning() {
+        var sequence = new Sequence(4, new int[]{1,2,3,4});
+        sequence.remove(2);
+        assertEquals(1, sequence.get(1));
+        assertEquals(3, sequence.get(2));
+        assertEquals(4, sequence.get(3));
+    }
+
+    @Test
+    public void removeShouldShrinkArrayWhenItGetsEmpty () {
+        var sequence = new Sequence(4, new int[]{1,2});
+        sequence.remove(2);
+        assertEquals(2, sequence.getCapacity());
+    }
+
+    @Test(expected=IllegalStateException.class)
+    public void extremaShouldRejectEmptySequences() {
+        var sequence = new Sequence(4, new int[]{});
+        sequence.extrema();
+    }
+
+    @Test
+    public void extremaShouldFindsBothMinAndMax () {
+        var sequence = new Sequence(10, new int[]{3,4,5,6,7,8});
+        var extrema = sequence.extrema();
+        assertEquals(3, extrema[0]);
+        assertEquals(8, extrema[1]);
+    }
+
+    @Test
+    public void extremaShouldFindsBothMinAndMaxWhenDecreasing () {
+        var sequence = new Sequence(10, new int[]{8,7,6,5,4,3});
+        var extrema = sequence.extrema();
+        assertEquals(3, extrema[0]);
+        assertEquals(8, extrema[1]);
+    }
+
+    @Test
+    public void extremaShouldFindsBothMinAndMaxWhenLengthIsOne () {
+        var sequence = new Sequence(10, new int[]{8});
+        var extrema = sequence.extrema();
+        assertEquals(8, extrema[0]);
+        assertEquals(8, extrema[1]);
+    }
+
+    @Test
+    public void shouldDetectNoDuplicates () {
+        var sequence = new Sequence(10, new int[]{1,2,3,4,5});
+        assertFalse(sequence.hasDuplicate());
+    }
+
+    @Test
+    public void shouldDetectDuplicates () {
+        var sequence = new Sequence(10, new int[]{1,5,3,4,5});
+        assertTrue(sequence.hasDuplicate());
+    }
+
+    @Test
+    public void hasDuplicateShouldAcceptEmptySequence () {
+        var sequence = new Sequence(10, new int[]{});
+        assertFalse(sequence.hasDuplicate());
+    }
+
 }
